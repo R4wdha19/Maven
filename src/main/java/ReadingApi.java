e45e81b49afe148afb99dc7062141eba91405d34
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-01-16 15:12:27 +0400

    Functions

--- src/main/java/ReadingApi.java
+++ src/main/java/ReadingApi.java
@@ -38,20 +38,59 @@ public class ReadingApi {
 		}
 	}
 
-	String webPage;
-	String country;
-	String name;
-	String code;
-	String domains;
+	static String newWebPage = "'http://www.Rawd.ac.uk/'";
+	static String newCountry = "'OMAN'";
+	static String newName = "'RAWDHA'";
+	static String newCode = "'1001'";
+	static String newState = "'SEEB'";
+	static String newDomains = "'Something'";
 
 	public static void main(String[] args) throws IOException, InterruptedException {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Select an option");
+		System.out.println("1 - Insert Into Table");
+		System.out.println("2 - Update A Row");
+		System.out.println("3 - Delet A Row");
+		System.out.println("4 - Read Table");
+		String option = sc.next();
+		int op = Integer.parseInt(option);
+		switch (op) {
+		case 1:
+			insertStatement();
+			break;
+		case 2:
+			updateStatement();
+			break;
+		case 3:
+			deleteStatement();
+			break;
+		case 4:
+			readStatement();
 
-		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.newBuilder()
-				.uri(URI.create("http://universities.hipolabs.com/search?country=United+States")).build();
-		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		}
 
-		String uglyJsonString = response.body();
+	}
+
+	public static void executingOfQurey(String sql) {
+
+		try {
+			Statement st = con.createStatement();
+			int executing = st.executeUpdate(sql);
+			System.out.println(sql);
+			if (executing > 0) {
+				System.out.println("Successful : " + sql);
+			} else {
+				System.out.println("Failed");
+			}
+		} catch (Exception ex) {
+
+			System.err.println(ex);
+		}
+
+	}
+
+	public static void readStatement() {
+		String uglyJsonString = url("http://universities.hipolabs.com/search?country=United+States");
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		JsonParser jp = new JsonParser();
 		JsonElement je = jp.parse(uglyJsonString);
@@ -64,65 +103,86 @@ public class ReadingApi {
 			String state = r.getState_province();
 			String code = r.getAlpha_two_code();
 			String domains = r.getDomins()[0];
-			String sqlQueryToInsert = insertStatement(webPage, country, name, state, code, domains);
-			String sqlQueryToRead = readStatement(webPage, country, name, state, code, domains);
-			String sqlQueryToUpdate = updateStatement(webPage, country, name, state, code, domains);
-			String sqlQueryToDelete = deleteStatement(webPage, country, name, state, code, domains);
-			executingOfQurey(sqlQueryToInsert);
+			String sqlQueryToRead = "SELECT * FROM ApiResults";
 			executingOfQurey(sqlQueryToRead);
-			executingOfQurey(sqlQueryToUpdate);
+		}
+	}
+
+	public static void updateStatement() {
+
+		System.out.println("Enter The Id To Be Updated ");
+		Integer userInput = sc.nextInt();
+		String uglyJsonString = url("http://universities.hipolabs.com/search?country=United+States");
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		JsonParser jp = new JsonParser();
+		JsonElement je = jp.parse(uglyJsonString);
+		String prettyJsonString = gson.toJson(je);
+		ColumnNamesClass[] names = gson.fromJson(prettyJsonString, ColumnNamesClass[].class);
+
+		String sqlQueryToUpdate = " Update  ApiResults SET web_pages= " + newWebPage + ",state_province= " + newState
+				+ ",alpha_two_code= " + newCode + " , name= " + newName + ",country= " + newCountry + ",domins= "
+				+ newDomains + "Where Id =" + userInput;
+		executingOfQurey(sqlQueryToUpdate);
+
+	}
+
+	public static void deleteStatement() {
+		String uglyJsonString = url("http://universities.hipolabs.com/search?country=United+States");
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		JsonParser jp = new JsonParser();
+		JsonElement je = jp.parse(uglyJsonString);
+		String prettyJsonString = gson.toJson(je);
+		ColumnNamesClass[] names = gson.fromJson(prettyJsonString, ColumnNamesClass[].class);
+		for (ColumnNamesClass r : names) {
+			String webPage = r.getWeb_pages()[0];
+			String country = r.getCountry();
+			String name = r.getName();
+			String state = r.getState_province();
+			String code = r.getAlpha_two_code();
+			String domains = r.getDomins()[0];
+			System.out.println("Please Enter The Id To Be Deleted ");
+			int idToBeDeleted = sc.nextInt();
+			String sqlQueryToDelete = "DELETE FROM ApiResults WHERE id = " + idToBeDeleted;
 			executingOfQurey(sqlQueryToDelete);
 
 		}
-
 	}
 
-	public static void executingOfQurey(String sql) {
+	public static void insertStatement() {
+		String uglyJsonString = url("http://universities.hipolabs.com/search?country=United+States");
 
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		JsonParser jp = new JsonParser();
+		JsonElement je = jp.parse(uglyJsonString);
+		String prettyJsonString = gson.toJson(je);
+		ColumnNamesClass[] names = gson.fromJson(prettyJsonString, ColumnNamesClass[].class);
+		for (ColumnNamesClass r : names) {
+			String webPage = r.getWeb_pages()[0];
+			String country = r.getCountry();
+			String name = r.getName();
+			String state = r.getState_province();
+			String code = r.getAlpha_two_code();
+			String domains = r.getDomins()[0];
+			String sqlQueryToInsert = " INSERT INTO ApiResults (web_pages,state_province,alpha_two_code , name,country ,domins)"
+					+ "VALUES('" + webPage + "','" + country + "', '" + name + "', '" + state + "','" + code + "', '"
+					+ domains + "')";
+			executingOfQurey(sqlQueryToInsert);
+
+		}
+	}
+
+	public static String url(String url) {
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+		HttpResponse<String> response = null;
 		try {
-			Statement st = con.createStatement();
-			int executing = st.executeUpdate(sql);
-			if (executing >= 0) {
-				System.out.println("Created Successfully : " + sql);
-			} else {
-				System.out.println("Creation Is Failed");
-			}
-		} catch (Exception ex) {
+			response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		} catch (Exception e) {
 
-			System.err.println(ex);
+			e.printStackTrace();
 		}
 
+		return response.body();
 	}
 
-	public static String insertStatement(String webPage, String country, String name, String state, String code,
-			String domains) {
-		String sqlQueryToInsert = " INSERT INTO ApiResults (web_pages,state_province,alpha_two_code , name,country ,domins)"
-				+ "VALUES('" + webPage + "','" + country + "', '" + name + "', '" + state + "','" + code + "', '"
-				+ domains + "')";
-		return sqlQueryToInsert;
-	}
-
-	public static String readStatement(String webPage, String country, String name, String state, String code,
-			String domains) {
-		String sqlQueryToRead = "SELECT * FROM ApiResults";
-		return sqlQueryToRead;
-	}
-
-	public static String updateStatement(String webPage, String country, String name, String state, String code,
-			String domains) {
-		String sqlQueryToUpdate = " Update  ApiResults SET (web_pages,state_province,alpha_two_code , name,country ,domins)"
-				+ "VALUES('" + webPage + "','" + country + "', '" + name + "', '" + state + "','" + code + "', '"
-				+ domains + "')";
-		return sqlQueryToUpdate;
-	}
-
-	public static String deleteStatement(String webPage, String country, String name, String state, String code,
-			String domains) {
-
-		System.out.println("Please Enter The Id To Be Deleted ");
-		int idToBeDeleted = sc.nextInt();
-
-		String sqlQueryToDelete = "DELETE FROM ApiResults WHERE id =" + idToBeDeleted;
-		return sqlQueryToDelete;
-	}
 }
