695b33ea8e59bcdac12f46bfd449e139cb327f92
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-01-17 10:09:18 +0400

    Classes

---
+++ src/main/java/Data.java
@@ -0,0 +1,36 @@
+
+public class Data {
+	Name name;
+	String[] tld;
+	String cca2;
+	String ccn3;
+	String cca3;
+	String cioc;
+	boolean independent;
+	String status;
+	boolean unMember;
+	Currencies currencies;
+	Idd idd;
+	String[] capital;
+	String[] altSpellings;
+	String region;
+	String subregion;
+	Languages languages;
+	Translations translations;
+	String[] latlng;
+	boolean landlocked;
+	Float area;
+	Demonyms demonyms;
+	String flag;
+	Maps maps;
+	Integer population;
+	String fifa;
+	Car car;
+	String[] timezones;
+	String[] continents;
+	Flags flags;
+	CoatOfArms coatOfArms;
+	String startOfWeek;
+	CapitalInfo capitalInfo;
+
+}
